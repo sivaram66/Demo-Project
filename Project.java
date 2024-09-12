f2f2c8709abcd7b30fd8257a3331f36a7e956794
Siva Ram <sivaramlikeu@gmail.com>
2024-09-12 12:22:15 +0530

    Added Main

---
+++ Project.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Project {
+public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("This is a Main Branch");
+    }
+}
