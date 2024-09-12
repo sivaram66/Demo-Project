e3776738d3ce9bc6f1b39b4753eb47cb905f2d6c
Siva Ram <sivaramlikeu@gmail.com>
2024-09-12 12:28:44 +0530

    add

--- Project.java
+++ Project.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Project {
-public static Scanner sc = new Scanner(System.in);
+public static Scanner sc1 = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("This is a Feature1");
     }
